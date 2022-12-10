e05af30fd377e4083a709de5948eeb2c30503e12
Erick-Andrade-Dev <erick.dev07@gmail.com>
2022-12-10 13:48:05 -0300

    criacao de rotas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom'
+import Home from './Pages/Home';
+import About from './Pages/About'
+import Projects from './Pages/Projects'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-main">
+      <div className="navegator">
+        <nav>
+          <img 
+            className="logo"
+            src="https://static.vecteezy.com/ti/fotos-gratis/p2/1325904-natureza-pequena-cachoeira-com-pedra-foto.jpg"></img>
+        </nav>
+        <nav>
+          <h2 className="title-portifolio">Meu portifólio</h2>
+        </nav>
+        <nav className="container-links">
+          <Link className="link" to="/">Principal</Link>
+          <Link className="link" to="/sobre">Sobre</Link>
+          <Link className="link" to="/projetos">Projetos</Link>
+        </nav>
+      </div>     
+      <Switch>  
+        <Route exact path="/" component={ Home }/>
+        <Route path="/sobre" component={ About }/>
+        <Route path="/projetos" component={ Projects }/>
+      </Switch> 
     </div>
   );
 }
---
+++ src/Pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home () {
+  return (
+    <div>
+      <main className="hero-image">
+        <img className="image-main" src="https://static.mundoeducacao.uol.com.br/mundoeducacao/conteudo_legenda/7f24684df8e24eb5c3ce142a91bc07e6.jpg" />
+      </main>
+    </div>
+  )
+}
