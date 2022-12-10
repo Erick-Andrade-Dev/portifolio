08a7ef2e545e6a11854e13d633be466411f119df
Erick-Andrade-Dev <erick.dev07@gmail.com>
2022-12-10 14:04:07 -0300

    criacao de rotas

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React from 'react';
+import logo from '../assets/logo'
 
-export default function Home () {
+export default function Home() {
   return (
     <div>
       <main className="hero-image">
-        <img className="image-main" src="https://static.mundoeducacao.uol.com.br/mundoeducacao/conteudo_legenda/7f24684df8e24eb5c3ce142a91bc07e6.jpg" />
+        <img className="image-main" src={logo}/>
       </main>
     </div>
   )
